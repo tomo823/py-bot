81011bf4e807d00f2977f7b81b656e48d48eff4c
HikaruEgashira <account@egahika.dev>
2023-10-01 18:16:21 +0900

    add vercel

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,6 @@
 
 curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 
+echo 'export PATH=$PATH:$(npm bin -g)' >> ~/.bashrc
 echo 'source "$HOME/.rye/env"' >> ~/.bashrc
 . "$HOME/.rye/env"

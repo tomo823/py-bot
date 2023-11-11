f3500acb68796c085a910e48d0fb1897ca08ffdd
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-12 03:52:26 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,8 @@ import json
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
-        query = json.loads(parsed_path.query)
+        # json might be cause of error
+        # query = json.loads(parsed_path.query)
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()

54ac736bd4cc57b2d483961d2807ceec9f3c2e42
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-12 04:11:00 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -7,10 +7,9 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         # json might be cause of error
-        query = parsed_path.query
+        query = str(parsed_path.query.strip("q="))
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write("Hello, world!".encode("utf-8"))
-        self.wfile.write(f"path: {query}".encode("utf-8"))
+        self.wfile.write(f"query: {query}".encode("utf-8"))
         return

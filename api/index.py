0149356ffddaffbd7adcf0388f2bc72e23e3ff81
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-12 03:45:36 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -2,21 +2,11 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 import json
 
+
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        # Parse query string
-        parsed_path = urlparse(self.path)
-        query = json.loads(parsed_path.query)
-
-        print("Query: " + query)
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write("hello world".encode("utf-8"))
-        return 
-        # self.send_response(200)
-        # self.send_header('Content-type','text/plain')
-        # self.end_headers()
-        # self.wfile.write('Hello, world!'.encode('utf-8'))
-        # return
+        self.wfile.write("Hello, world!".encode("utf-8"))
+        return

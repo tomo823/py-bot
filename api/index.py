b8a831f31d5a7dc9993381b6ae1084812cdd0676
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-12 10:45:47 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,14 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
-import json
-from respond import environment_setting, default_setting, folder_list, openai, pinecone, vector_store
+import json, sys
+from pathlib import Path
+#add path to respond.py which is in the parent directory
+
+current_dir = Path(__file__)
+parent_dir = current_dir.parent
+sys.path.append(str(parent_dir))
+
+from Laf import respond
 
 
 class handler(BaseHTTPRequestHandler):
@@ -9,9 +16,10 @@ class handler(BaseHTTPRequestHandler):
         #get query from url and strip "q="
         parsed_path = urlparse(self.path)
         query = str(parsed_path.query.strip("q="))
-
+        #get response from respond.py
+        response = respond.get_query(query)
         self.send_response(200)
         self.send_header("Content-type", "text/plain; charset=utf-8")
         self.end_headers()
-        self.wfile.write(f"あかさたな {query}".encode("utf-8"))
+        self.wfile.write(f"{response}".encode("utf-8"))
         return

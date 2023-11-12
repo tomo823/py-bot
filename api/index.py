e6b84fe7a2c10e7abb69471288f8bab5d02bfe5e
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-12 09:15:57 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ class handler(BaseHTTPRequestHandler):
         query = str(parsed_path.query.strip("q="))
 
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", "text/plain; charset=utf-8")
         self.end_headers()
         self.wfile.write(f"あかさたな {query}".encode("utf-8"))
         return

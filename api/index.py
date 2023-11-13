74987fca3386c55c333fb660800a95dc45793f80
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-14 04:37:41 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ import json, sys, os
 p = Path()
 sys.path.append(os.path.join(str(p.cwd().resolve()), "Laf"))
 
-file = os.listdir(p.cwd())
+file = os.listdir(os.path.join(str(p.cwd().resolve()), "Laf"))
 from Laf import respond
 
 # import pinecone

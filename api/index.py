8321530366696ccc7360f8ec657ca4dc07718eb2
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-14 06:29:44 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -122,7 +122,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # get query from url and strip "q="
         parsed_path = urlparse(self.path)
-        query = str(parsed_path.query.strip("q="))
+        query = str(parsed_path.query)
 
         self.send_response(200)
         self.send_header("Content-type", "text/plain; charset=utf-8")

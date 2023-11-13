b696e14957825f5f591ce0dfc51f8a1c22b0b4e7
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-14 04:43:20 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ import json, sys, os
 p = Path()
 sys.path.append(os.path.join(str(p.cwd().resolve()), "Laf"))
 
-file = os.listdir(os.path.join(str(p.cwd().resolve()), "Laf"))
+file = os.listdir(os.path.join(str(p.cwd().resolve()), "Laf", ".git"))
 # from Laf import respond
 
 # import pinecone

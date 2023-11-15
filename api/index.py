f08cf8c4e73f9fc61dbc9fbfa08b4edfcc8cead8
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-11-15 22:33:50 +0900

    modified:   api/index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
-from urllib.parse import urlparse
+from urllib.parse import urlparse, quote
+
 # from urllib.parse import parse_qs, urlencode, quote, unquote
 # from pathlib import Path
 # import json, sys, os, re, glob, urllib.parse, time
@@ -17,9 +18,10 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         query = str(parsed_path.query).strip("q=")
+        encoded_text = quote(query)
 
         self.send_response(200)
         self.send_header("Content-type", "text/plain; charset=utf-8")
         self.end_headers()
-        self.wfile.write(f"question: {query}".encode("utf-8"))
+        self.wfile.write(f"question: {encoded_text}".encode("utf-8"))
         return
